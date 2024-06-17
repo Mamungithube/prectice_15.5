b4f7cba8f6f2a1a2177c9a5ef67a1520af191f84
mamungithube <orroshidmdmamun50@gmail.com>
2024-06-18 00:10:21 +0600

    commit

--- Album/forms.py
+++ Album/forms.py
@@ -5,3 +5,8 @@ class Albumform(forms.ModelForm):
     class Meta: 
         model = Album
         fields = '__all__'
+
+        widgets = {
+            'Rating': forms.Select(choices=[(i,i) for i in range(1,6)]),
+            'Album_release_date': forms.NumberInput(attrs={'type':'date'}),
+        }
